e896ca00f2298d67fe4f7ee8ee82a5c253707de4
Aklyny <sri1999bharathi@gmail.com>
2020-10-08 16:45:53 +0530

    hero

---
+++ src/components/InfoSection/data.js
@@ -0,0 +1,12 @@
+export const homeObjOne = {
+    id:'about',
+    lightBg:false,
+    lightText:true,
+    lightTextDesc:true,
+    topLine:'Aklyny Tech',
+    headline:'Get Customized Website for Business at Cheaper Price',
+    description:'Customized React Web Application is availabe 2000 INR with the Source code attached, Customizing web is free if you bought the Template',
+    buttonLable:'Get Started',
+    imgStart:false,
+    img:require()
+}
